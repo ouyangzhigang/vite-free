ec2778dd9bcded0d22a13093905df99f3e7d3ea1
Voyagergle <ouyangzhigang@live.com>
2023-02-13 01:32:47 +0800

    env init

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import { setupCounter } from './counter'
 
-import App from './App'
-import router from './router'
-// import Worker from './worker?worker'
+console.log(import.meta.env)
 
-import './assets/main.css'
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
 
-// const worker = new Worker()
-// worker.onmessage = function (e) {
-//   console.log(e)
-// }
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
