400b89fbcf8bbc7789bcfff6dd365ed387320a0a
zaneouyang <zaneouyang@lexin.com>
2025-05-09 08:13:23 +0800

    feat: vue form valid

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.esm-bundler.js'
+    }
+  },
   build: {
     manifest: true,
     rollupOptions: {
